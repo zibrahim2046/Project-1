01f68b98f7d785112876abcdcfc5de1102a44281
cshanebrown78 <cshanebrown78@gmail.com>
2020-03-19 20:05:29 -0500

    added jumbotron and what to bring card on planet.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,10 @@ var planets = {
     daysPerYear: "88 Earth days",
     link: "https://nineplanets.org/mercury/",
     size: "7 times less surface area than Earth",
-    type: "Rocky, Terrestrial"
+    type: "Rocky, Terrestrial",
+    distance: "78 million miles",
+    travelTime: "130 days",
+    name: "Mercury"
   },
   venus: {
     surfaceTempMin: "870",
@@ -17,7 +20,10 @@ var planets = {
     daysPerYear: "225 Earth days",
     link: "https://nineplanets.org/venus/",
     size: "10% less surface area than Earth",
-    type: "Rocky, Terrestrial"
+    type: "Rocky, Terrestrial",
+    distance: "69 million miles",
+    travelTime: "115 days",
+    name: "Venus"
   },
   earth: {
     surfaceTempMin: "-129",
@@ -27,17 +33,21 @@ var planets = {
     daysPerYear: "365 Earth days",
     link: "https://nineplanets.org/earth/",
     size: "",
-    type: "Rocky, Terrestrial"
+    type: "Rocky, Terrestrial",
+    
   },
   mars: {
     surfaceTempMin: "-195",
     surfaceTempMax: "70",
-    rotation: "1 Earth day and 6 hours",
-    dayLength: "1 Earth day and 6 hours",
+    rotation: "1 Earth day and 6hours",
+    dayLength: "1 Earth day and 6hours",
     daysPerYear: "669 Earth days",
     link: "https://nineplanets.org/mars/",
     size: "Just over 1/2 the surface area of Earth",
-    type: "Rocky, Terrestrial"
+    type: "Rocky, Terrestrial",
+    distance: "145 million miles",
+    travelTime: "241 days",
+    name: "Mars"
   },
   jupiter: {
     surfaceTempMin: "(Atmosphere) -229",
@@ -47,7 +57,10 @@ var planets = {
     daysPerYear: "12 Earth years",
     link: "https://nineplanets.org/jupiter/",
     size: "122 times more surface area than Earth",
-    type: "Gas Giant"
+    type: "Gas Giant",
+    distance: "512 million miles",
+    travelTime: "853 days",
+    name: "Jupiter"
   },
   saturn: {
     surfaceTempMin: "(Atmosphere) -288",
@@ -57,7 +70,10 @@ var planets = {
     daysPerYear: "29.5 Earth years",
     link: "https://nineplanets.org/saturn/",
     size: "83 times more surface area than Earth",
-    type: "Gas Giant"
+    type: "Gas Giant",
+    distance: "973 million miles",
+    travelTime: "1621 days",
+    name: "Saturn"
   },
   uranus: {
     surfaceTempMin: "(Atmosphere) -371",
@@ -65,7 +81,10 @@ var planets = {
     daysPerYear: "84 Earth years",
     link: "https://nineplanets.org/uranus/",
     size: "16 times more surface area than Earth",
-    type: "Ice Giant"
+    type: "Ice Giant",
+    distance: "1.9 billion miles",
+    travelTime: "3166 days",
+    name: "Uranus"
   },
   neptune: {
     surfaceTempMin: "(Atmosphere) -360",
@@ -75,7 +94,10 @@ var planets = {
     daysPerYear: "165 Earth years",
     link: "https://nineplanets.org/neptune/",
     size: "15 times more surface area than Earth",
-    type: "Ice Giant"
+    type: "Ice Giant",
+    distance: "2.8 billion miles",
+    travelTime: "4666 days",
+    name: "Neptune"
   },
   pluto: {
     surfaceTempMin: "-400",
@@ -85,7 +107,10 @@ var planets = {
     daysPerYear: "248 Earth years",
     link: "https://nineplanets.org/pluto/",
     size: "30 times less surface area than Earth",
-    type: "Dwarf"
+    type: "Dwarf",
+    distance: "3.2 billion miles",
+    travelTime: "5333 days",
+    name: "Pluto"
   }
 };
 
@@ -157,12 +182,14 @@ function secondAPI() {
 function imgPush() {
 // $(document).on("click",".imgPic",function() {
   planetImg = JSON.parse(localStorage.getItem("planetImg"));
-  console.log(planetImg);
   userPlanetStored = localStorage.getItem("userPlanet");
   
+  var jTron = $("<h1 class = 'display-4'>")
+  jTron.text(planets[userPlanetStored].name);
+  $(".for-jtron").append(jTron);
+
   for (var j = 0; j < 4; j++) {
     
-    console.log(planetImg[j]);
     var picDiv = $("<div class = 'planet-imgs'>");
     var planetImgTag = $("<img class = 'display-images'>");
     planetImgTag.attr("src", planetImg[j]);
@@ -220,8 +247,6 @@ function imgPush() {
     var aphelionDisMiles = parseInt(localStorage.getItem("aphelion") * .621371);
     var y = aphelionDisMiles.toLocaleString();
     aphelionPar.prepend("Furthest distance from the Sun:  " + y + " Miles");
-
-
     factsDiv.append(aphelionPar);
 
     var perihelionPar = $("<p class = 'facts'>");
@@ -230,10 +255,16 @@ function imgPush() {
     perihelionPar.prepend("Shortest distance from the Sun:  " + x + " Miles");
     factsDiv.append(perihelionPar);
 
-    var planetLink = $("<p class ='facts'>");
-    h = planets[userPlanetStored].link
-    planetLink.prepend("Type of Planet:  " + h);
-    factsDiv.append(planetType);
+    var planetLink = $("<a>");
+    link = planets[userPlanetStored].link;
+    console.log(link);
+    planetLink.attr("href", link);
+    planetLink.text("More Facts");
+    planetLink.addClass("link");
+
+    factsDiv.append(planetLink);
+
+
 
     $(".theFacts").prepend(factsDiv)
 // })
